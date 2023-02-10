461614842d2591669e248aaedd149fdcdce391e9
Tanapoom Sinpaksa <tanapoom@exzyteam.com>
2023-02-10 12:26:49 +0700

    =edited docker-compose.yml

--- db-conection.js
+++ db-conection.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize("storedata", "root", "password", {
-  host: "127.0.0.1",
+  host: "mysql_server",
   dialect: "mysql",
   port: 3306,
   logging: false,
@@ -9,12 +9,22 @@ const sequelize = new Sequelize("storedata", "root", "password", {
 }
 });
 
+// const sequelize = new Sequelize('postgres://root:password@postgres_server:5432/storedata');
+
 const connectToMySQL = async () => {
+  let retries = 0;
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
+    if (retries <= 3) {
+      setTimeout(async() => {
+        await sequelize.authenticate();
+      }, 5000);
+      retries++;
+    }
+    else throw error;
   }
 };
 
